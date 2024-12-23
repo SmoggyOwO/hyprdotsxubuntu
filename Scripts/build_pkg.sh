5b67fbd3d915da53f21a3ccb3f22c51f5baa9946
SmoggyOwO <dashing4149@gmail.com>
2024-12-23 06:47:47 +0530

    Reordered the built packages to ensure no mistakes

--- Scripts/build_pkg.sh
+++ Scripts/build_pkg.sh
@@ -9,6 +9,13 @@ set -e  # Exit immediately if a command exits with a non-zero status
 
 HOME_DIR=$HOME
 
+# Function to check if the last command was successful
+check_success() {
+    if [ $? -ne 0 ]; then
+        echo "Error: Failed to build $1"
+        exit 1
+    fi
+}
 
 install_dependencies() {
     # Enable multiverse and universe repositories
@@ -79,7 +86,12 @@ install_dependencies() {
         libtomlplusplus3 \
         libzip-dev \
         gir1.2-rsvg-2.0 \
-        libtomlplusplus-dev
+        libtomlplusplus-dev \
+        libwlroots-dev \
+        wayland-protocols \
+        libglvnd-dev \
+        libsystemd-dev \
+        libdisplay-info-dev
 }
 
 # Function to build and install a package
@@ -87,6 +99,7 @@ build_and_install() {
     cd "$HOME_DIR"
     local repo_url=$1
     local build_commands=$2
+    local package_name=$3
 
     echo "Cloning $repo_url..."
     git clone --depth 1 --no-single-branch --tags "$repo_url"
@@ -96,6 +109,7 @@ build_and_install() {
     git checkout $(git describe --tags $(git rev-list --tags --max-count=1))
     echo "Building $repo_name..."
     eval "$build_commands"
+    check_success "$package_name"
 
     echo "Finished $repo_name. Returning to home directory."
     cd "$HOME_DIR"
@@ -107,69 +121,83 @@ build_and_install() {
 
 install_dependencies
 
-# 1. Build swww
-build_and_install "https://github.com/LGFae/swww" \
-"cargo build --release && sudo cp target/release/swww /usr/local/bin/ && sudo cp target/release/swww-daemon /usr/local/bin/"
-
-# 2. Build rofi
-build_and_install "https://github.com/lbonn/rofi" \
-"meson setup build && ninja -C build && ninja -C build install"
-
-# 3. Build swaylock-effects
-build_and_install "https://github.com/mortie/swaylock-effects" \
-"meson build && ninja -C build && sudo ninja -C build install"
-
-#4. Build sdbus-cpp
+# 1. Build sdbus-cpp
 build_and_install "https://github.com/Kistler-Group/sdbus-cpp" \
-"mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release ${OTHER_CONFIG_FLAGS} && cmake --build . && sudo cmake --build . --target install"
+"mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release ${OTHER_CONFIG_FLAGS} && cmake --build . && sudo cmake --build . --target install" \
+"sdbus-cpp"
 
-# 5. Build hyprutils
-build_and_install "https://github.com/hyprwm/hyprutils" \
-"cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=/usr -S . -B ./build && cmake --build ./build --config Release --target all -j`nproc 2>/dev/null || getconf NPROCESSORS_CONF` && sudo cmake --install build"
-
-# 6. Build hyprwayland-scanner
-build_and_install "https://github.com/hyprwm/hyprwayland-scanner" \
-"cmake -DCMAKE_INSTALL_PREFIX=/usr -B build && cmake --build build -j `nproc` && sudo cmake --install build"
-
-# 7. Build hyprlang
+# 2. Build hyprlang
 build_and_install "https://github.com/hyprwm/hyprlang" \
-"cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=/usr -S . -B ./build && cmake --build ./build --config Release --target hyprlang -j`nproc 2>/dev/null || getconf _NPROCESSORS_CONF` && sudo cmake --install ./build"
+"cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=/usr -S . -B ./build && cmake --build ./build --config Release --target hyprlang -j\`nproc 2>/dev/null || getconf _NPROCESSORS_CONF\` && sudo cmake --install ./build" \
+"hyprlang"
 
-# 8. Build hyprland-protocols
+# 3. Build hyprland-protocols
 build_and_install "https://github.com/hyprwm/hyprland-protocols" \
-"meson setup build && ninja -C build && ninja -C build install"
+"meson setup build && ninja -C build && ninja -C build install" \
+"hyprland-protocols"
 
-# 9. Build hyprgraphics
+# 4. Build hyprwayland-scanner
+build_and_install "https://github.com/hyprwm/hyprwayland-scanner" \
+"cmake -DCMAKE_INSTALL_PREFIX=/usr -B build && cmake --build build -j \`nproc\` && sudo cmake --install build" \
+"hyprwayland-scanner"
+
+# 5. Build hyprgraphics
 build_and_install "https://github.com/hyprwm/hyprgraphics" \
-"cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=/usr -S . -B ./build && cmake --build ./build --config Release --target all -j`nproc 2>/dev/null || getconf NPROCESSORS_CONF` && sudo cmake --install build"
+"cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=/usr -S . -B ./build && cmake --build ./build --config Release --target all -j\`nproc 2>/dev/null || getconf NPROCESSORS_CONF\` && sudo cmake --install build" \
+"hyprgraphics"
 
-# 10. Build hyprcursor
+# 6. Build hyprcursor
 build_and_install "https://github.com/hyprwm/hyprcursor" \
-"cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=/usr -S . -B ./build && cmake --build ./build --config Release --target all -j`nproc 2>/dev/null || getconf _NPROCESSORS_CONF` && sudo cmake --install build"
+"cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=/usr -S . -B ./build && cmake --build ./build --config Release --target all -j\`nproc 2>/dev/null || getconf _NPROCESSORS_CONF\` && sudo cmake --install build" \
+"hyprcursor"
 
-# 11. Build aquamarine
-build_and_install "https://github.com/hyprwm/aquamarine" \
-"cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=/usr -S . -B ./build && cmake --build ./build --config Release --target all -j`nproc 2>/dev/null || getconf _NPROCESSORS_CONF`"
+# 7. Build hyprutils
+build_and_install "https://github.com/hyprwm/hyprutils" \
+"cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=/usr -S . -B ./build && cmake --build ./build --config Release --target all -j\`nproc 2>/dev/null || getconf NPROCESSORS_CONF\` && sudo cmake --install build" \
+"hyprutils"
 
-# 12. Build hyprpicker
+# 8. Build swww
+build_and_install "https://github.com/LGFae/swww" \
+"cargo build --release && sudo cp target/release/swww /usr/local/bin/ && sudo cp target/release/swww-daemon /usr/local/bin/" \
+"swww"
+
+# 9. Build rofi
+build_and_install "https://github.com/lbonn/rofi" \
+"meson setup build && ninja -C build && ninja -C build install" \
+"rofi"
+
+# 10. Build swaylock-effects
+build_and_install "https://github.com/mortie/swaylock-effects" \
+"meson build && ninja -C build && sudo ninja -C build install" \
+"swaylock-effects"
+
+# 11. Build hyprpicker
 build_and_install "https://github.com/hyprwm/hyprpicker" \
-"cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=/usr -S . -B ./build && cmake --build ./build --config Release --target hyprpicker -j\`nproc 2>/dev/null || getconf _NPROCESSORS_CONF\` && sudo cmake --install ./build"
+"cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=/usr -S . -B ./build && cmake --build ./build --config Release --target hyprpicker -j\`nproc 2>/dev/null || getconf _NPROCESSORS_CONF\` && sudo cmake --install ./build" \
+"hyprpicker"
+
+# 12. Build aquamarine
+build_and_install "https://github.com/hyprwm/aquamarine" \
+"cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=/usr -S . -B ./build && cmake --build ./build --config Release --target all -j\`nproc 2>/dev/null || getconf _NPROCESSORS_CONF\`" \
+"aquamarine"
 
 # 13. Build ImageMagick
 build_and_install "https://github.com/ImageMagick/ImageMagick" \
-"./configure && make && sudo make install"
+"./configure && make && sudo make install" \
+"ImageMagick"
 
 # 14. Build xdg-desktop-portal-hyprland
 build_and_install "https://github.com/hyprwm/xdg-desktop-portal-hyprland" \
-"cmake -DCMAKE_INSTALL_LIBEXECDIR=/usr/lib -DCMAKE_INSTALL_PREFIX=/usr -B build && cmake --build build && sudo cmake --install build"
+"cmake -DCMAKE_INSTALL_LIBEXECDIR=/usr/lib -DCMAKE_INSTALL_PREFIX=/usr -B build && cmake --build build && sudo cmake --install build" \
+"xdg-desktop-portal-hyprland"
 
 # 15. Build hyprland
 build_and_install "https://github.com/hyprwm/Hyprland" \
-"make all && sudo make install"
+"make all && sudo make install" \
+"Hyprland"
 
 #--------------------------------------#
 # Completion message                   #
 #--------------------------------------#
 
 echo "All packages have been built and installed successfully."
-
