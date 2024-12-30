cd43ae12fa1ebd82d4155da9adbcd0d7d7dbb429
SmoggyOwO <dashing4149@gmail.com>
2024-12-30 12:27:58 +0530

    Add sddm config in post install script

--- Scripts/install_pst.sh
+++ Scripts/install_pst.sh
@@ -30,49 +30,49 @@ prompt_timer() {
     fi
 }
 
-# SDDM Configuration
-# if pkg_installed sddm; then
-#     echo -e "\033[0;32m[DISPLAYMANAGER]\033[0m detected // sddm"
-#
-#     if [ ! -d /etc/sddm.conf.d ]; then
-#         sudo mkdir -p /etc/sddm.conf.d
-#     fi
-#
-#     if [ ! -f /etc/sddm.conf.d/kde_settings.t2.bkp ]; then
-#         echo -e "\033[0;32m[DISPLAYMANAGER]\033[0m configuring sddm..."
-#         echo -e "Select sddm theme:\n[1] Candy\n[2] Corners"
-#         read -p " :: Enter option number : " sddmopt
-#
-#         case $sddmopt in
-#             1) sddmtheme="Candy" ;;
-#             *) sddmtheme="Corners" ;;
-#         esac
-#
-#         # Check if themes directory exists, create if not
-#         sudo mkdir -p /usr/share/sddm/themes/
-#
-#         # Extract theme
-#         sudo tar -xzf "${scrDir}/Source/arcs/Sddm_${sddmtheme}.tar.gz" -C /usr/share/sddm/themes/
-#
-#         # Create and backup config
-#         sudo touch /etc/sddm.conf.d/kde_settings.conf
-#         sudo cp /etc/sddm.conf.d/kde_settings.conf /etc/sddm.conf.d/kde_settings.t2.bkp
-#         sudo cp /usr/share/sddm/themes/${sddmtheme}/kde_settings.conf /etc/sddm.conf.d/
-#     else
-#         echo -e "\033[0;33m[SKIP]\033[0m sddm is already configured..."
-#     fi
-#
-#     # Set user avatar
-#     if [ ! -f /usr/share/sddm/faces/${USER}.face.icon ] && [ -f "${scrDir}/Source/misc/${USER}.face.icon" ]; then
-#         sudo mkdir -p /usr/share/sddm/faces/
-#         sudo cp "${scrDir}/Source/misc/${USER}.face.icon" /usr/share/sddm/faces/
-#         echo -e "\033[0;32m[DISPLAYMANAGER]\033[0m avatar set for ${USER}..."
-#     fi
-# else
-#     echo -e "\033[0;33m[WARNING]\033[0m sddm is not installed..."
-# fi
+#SDDM Configuration
+if pkg_installed sddm; then
+    echo -e "\033[0;32m[DISPLAYMANAGER]\033[0m detected // sddm"
 
-# Dolphin Configuration
+    if [ ! -d /etc/sddm.conf.d ]; then
+        sudo mkdir -p /etc/sddm.conf.d
+    fi
+
+    if [ ! -f /etc/sddm.conf.d/kde_settings.t2.bkp ]; then
+        echo -e "\033[0;32m[DISPLAYMANAGER]\033[0m configuring sddm..."
+        echo -e "Select sddm theme:\n[1] Candy\n[2] Corners"
+        read -p " :: Enter option number : " sddmopt
+
+        case $sddmopt in
+            1) sddmtheme="Candy" ;;
+            *) sddmtheme="Corners" ;;
+        esac
+
+        # Check if themes directory exists, create if not
+        sudo mkdir -p /usr/share/sddm/themes/
+
+        # Extract theme
+        sudo tar -xzf "${scrDir}/../Source/arcs/Sddm_${sddmtheme}.tar.gz" -C /usr/share/sddm/themes/
+
+        # Create and backup config
+        sudo touch /etc/sddm.conf.d/kde_settings.conf
+        sudo cp /etc/sddm.conf.d/kde_settings.conf /etc/sddm.conf.d/kde_settings.t2.bkp
+        sudo cp /usr/share/sddm/themes/${sddmtheme}/kde_settings.conf /etc/sddm.conf.d/
+    else
+        echo -e "\033[0;33m[SKIP]\033[0m sddm is already configured..."
+    fi
+
+    # Set user avatar
+    if [ ! -f /usr/share/sddm/faces/${USER}.face.icon ] && [ -f "${scrDir}/Source/misc/${USER}.face.icon" ]; then
+        sudo mkdir -p /usr/share/sddm/faces/
+        sudo cp "${scrDir}/Source/misc/${USER}.face.icon" /usr/share/sddm/faces/
+        echo -e "\033[0;32m[DISPLAYMANAGER]\033[0m avatar set for ${USER}..."
+    fi
+else
+    echo -e "\033[0;33m[WARNING]\033[0m sddm is not installed..."
+fi
+
+#Dolphin Configuration
 if pkg_installed dolphin && pkg_installed xdg-utils; then
     echo -e "\033[0;32m[FILEMANAGER]\033[0m detected // dolphin"
     xdg-mime default org.kde.dolphin.desktop inode/directory
